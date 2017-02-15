c465b4246bf02b37004ba6617d986396fbea8dbd
Kelly Moore <kellygarskemoore@gmail.com>
2017-02-15 10:35:50 -0600

    js interview question of sorting an array in ascending order

--- app/main.js
+++ app/main.js
@@ -4,4 +4,25 @@
       .platformBrowserDynamic()
       .bootstrapModule(app.AppModule);
   });
+
+var numArray = [4,2,9,12,1,5,3,7];
+var newNumArray = [];
+var num;
+
+for(i=0; i<20; i++) {
+	console.log("i: ", i);
+	for(j=0; j<numArray.length; j++) {
+		console.log("j: ", j);
+		num = numArray[j];
+		console.log("num: ", num);
+		if (num === i) {
+			newNumArray.push(num);
+			console.log("i is: ", i, "num is: ", num, "and newNumArray: ", newNumArray);
+		} else {
+			console.log("In index j else!");
+		}
+	}
+}
+console.log("newNumArray: ", newNumArray);
+
 })(window.app || (window.app = {}));
